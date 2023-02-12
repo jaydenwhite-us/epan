bcc2515122674b98b9adbeea6782067918f58784
jaydenwhite-us <jaydenwhite@bennington.edu>
2023-02-12 15:52:07 -0600

    Ignore target

--- target/debug/cross_compilation_test.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jayde\Desktop\Continuing\ Education\Rust\epan\target\debug\cross_compilation_test.exe: C:\Users\jayde\Desktop\Continuing\ Education\Rust\epan\cross_compilation_test\src\main.rs
--- target/debug/deps/cross_compilation_test.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jayde\Desktop\Continuing Education\Rust\epan\target\debug\deps\cross_compilation_test.exe: cross_compilation_test\src\main.rs
-
-C:\Users\jayde\Desktop\Continuing Education\Rust\epan\target\debug\deps\cross_compilation_test.d: cross_compilation_test\src\main.rs
-
-cross_compilation_test\src\main.rs:
